2610d68d399a8c945118304ab3b7a8075d6ace3a
schentea <schentea@gmail.com>
2024-02-29 10:46:26 +0900

    asd

--- src/index.js
+++ src/index.js
@@ -24,4 +24,19 @@ app.get("/", (req, res) => res.send({ name: "kenJo" }));
 app.use("/notice", noticeRouter);
 app.use("/apple", appleRouter);
 
+app.get("/people", (req, res) => {
+  fetch(
+    `https://c.q-net.or.kr/openapi/Ncs1info/ncsinfo.do?type=json&pageNo=1&numOfRows=10&ServiceKey=GfEBGZl/WVWZaww3GJjoLpRrE++wLFKbom/Sth6vxaX+A8qKV6FBHeMZe0zjSNjGSizunxh5Ylj4ZNegI83s9w==`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      res.json(data);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      res.status(500).json({ error: "Error fetching data from Neople API" });
+    });
+});
+
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
